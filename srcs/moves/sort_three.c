556fe587f8fe1375c76ed1b7fa8c27225bb82d2e
NamSaga <sagarandria@gmail.com>
2025-01-20 12:41:18 +0300

    push-swap

---
+++ srcs/moves/sort_three.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_three.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmamisoa <rmamisoa@student.42antananarivo  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/20 11:56:30 by rmamisoa          #+#    #+#             */
+/*   Updated: 2025/01/20 11:56:44 by rmamisoa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../../inc/push_swap.h"
+
+void sort_three(t_stack_node **a) {
+    t_stack_node *biggest_node;
+
+    biggest_node = find_max(*a);
+    if (biggest_node == *a)
+        ra(a, false);
+    else if ((*a)->next == biggest_node)
+        rra(a, false);
+    if ((*a)->nbr > (*a)->next->nbr)
+        sa(a, false);
+}
+
